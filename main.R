3a7355e6966e5f61781812f8bd246c1f2977fb3d
amaurel <alexandre.maurel@gmail.com>
2017-11-14 14:40:48 +0100

    rename

--- main.R
+++ main.R
@@ -3,12 +3,12 @@ library(dplyr)
 library(reshape2)
 
 (ctx = tercenCtx())  %>% 
-  select(.cindex, .rindex, .values) %>% 
-  reshape2::acast(.cindex ~ .rindex, value.var='.values', fun.aggregate=mean) %>%
+  select(.ci, .ri, .y) %>% 
+  reshape2::acast(.ci ~ .ri, value.var='.y', fun.aggregate=mean) %>%
   prcomp(scale=FALSE, center=FALSE, na.action=na.omit) %>%
   predict() %>%
   as_tibble() %>%
-  mutate(.cindex = seq_len(nrow(.))-1) %>%
+  mutate(.ci = seq_len(nrow(.))-1) %>%
   ctx$addNamespace() %>%
   ctx$save()
 
